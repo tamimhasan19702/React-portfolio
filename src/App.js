62f85ccc5ed59a582bbfdb9caddf2456cbeac147
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-19 18:30:24 +0600

    Skills component create and carousal add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import NavMenu from './components/NavMenu/NavMenu';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './components/Banner/Banner';
+import { Skills } from './components/Skills/Skills';
 
 const App = () => {
   return (
     <div className='App'>
       <NavMenu />
       <Banner />
+      <Skills/>
     </div>
   )
 }
