4e344b47022e29fcb638072072cb2843fc04bbe6
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-19 18:06:08 +0600

    looping slider add

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,43 @@
+import { useState, useEffect } from 'react';
 import { Container, Row ,Col} from "react-bootstrap";
-import { ArrowRightCircle } from "react-bootstrap"
+import { ArrowRightCircle } from "react-bootstrap";
+import headerImg from '../../assets/img/header-img.svg';
 
 export const Banner = () => {
+
+    const [loopNum, setLoopNum ] = useState(0);
+    const [ isDeleting, setIsDeleting ] = useState(false);
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer" ];
+    const [ text, setText ] = useState('');
+    const [ delta,setData ] = useState(300- Math.random()*100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        },delta)
+     return () => {clearInterval(ticker)}; 
+    },[text])
+
+    const tick = () 
+
+
     return (
         <section className="banner" id="home">
          <Container>
             <Row className="align-items-center">
+                
                 <Col xs={12} md={6} xl={7}>
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>{`Hi, I am Tamim Hasan`} <span className="wrap">Web Developer</span></h1>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores consequatur, voluptas laboriosam perspiciatis assumenda eaque ratione nihil </p>
                   <button onClick={() => console.log(`connect`)}>Let's Connect</button>
                 </Col>
+
+               <Col xs={12} md={6} xl={5}>
+               <img src={headerImg} alt="Header Img" />
+               </Col>
+
             </Row>
          </Container>
         </section>
