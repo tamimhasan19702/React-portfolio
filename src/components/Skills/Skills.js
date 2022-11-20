f3c5750a03c6a057502e9fb09427699ddef181cc
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-20 19:23:34 +0600

    skills slider updates

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,10 @@
+import { Container, Col, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/style.css";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from '../../assets/img/meter1.svg';
+import meter2 from '../../assets/img/meter2.svg';
+import meter3 from '../../assets/img/meter3.svg';
+import colorSharp from '../../assets/img/color-sharp.png';
 
 export const Skills = () => {
 const responsive = {
@@ -21,4 +26,46 @@ const responsive = {
     }
 
 };    
+
+
+return(
+    <section className="skills" id="skills">
+      <Container>
+        <Row>
+            <Col>
+            <div className="skill-bx">
+                <h2>
+                    Skills
+                </h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br></br>Animi nobis ipsam dolorem officiis nisi consequuntur totam quos.
+                </p>
+                <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                <div className="item">
+                    <img src={meter1} alt="Image" />
+                    <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                    <img src={meter2} alt="Image" />
+                    <h5>Brand Identity</h5>
+                </div>
+                <div className="item">
+                    <img src={meter3} alt="Image" />
+                    <h5>Logo Design</h5>
+                </div>
+                <div className="item">
+                    <img src={meter1} alt="Image" />
+                    <h5>Web Development</h5>
+                </div>
+                </Carousel>
+            </div>
+            </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp}/>
+    </section>
+)
+
+
+
 }
