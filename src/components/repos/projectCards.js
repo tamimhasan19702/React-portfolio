e31648e287c8c8b6b50bc538221d0637404a8399
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-22 21:48:28 +0600

    project card component update

--- src/components/repos/projectCards.js
+++ src/components/repos/projectCards.js
@@ -1,13 +1,15 @@
-export const projectCard = ({title,description,imgUrl}) => [
+import { Col } from "react-bootstrap"
+
+export const projectCard = ({title,description,imgUrl}) => {
     return (
         <Col sm={6} md={4}>
         <div className="proj-imgbx">
         <img src={imgUrl} alt="" />
         <div className="proj-tx">
             <h4>{title}</h4>
-            <span></span>
+            <span>{description}</span>
         </div>
         </div>
         </Col>
     )
-]
+}
