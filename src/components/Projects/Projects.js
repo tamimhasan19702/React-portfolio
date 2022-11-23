0a7fd10a3a52c6048511014d94bf0a3e68a128c3
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-23 18:52:37 +0600

    building projects components

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,56 @@
+import { Container, Row, Col, Nav } from "react-bootstrap";
+import projImg1 from '../../assets/img/project-img1.png';
+import projImg2 from '../../assets/img/project-img2.png';
+import projImg3 from '../../assets/img/project-img3.png';
+
 export const Projects = () => {
     
     const projects = [
         {
-            title: "Bussiness Startup"
-        }
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
     ]
     
     return (
+    <section className="project" id="project">
+       <Container>
+        <Row>
+            <Col>
+            <h2>Projects</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur labore aut debitis sed nobis at.</p>
+            <Nav>
 
+            </Nav>
+            </Col>
+        </Row>
+       </Container>
+    </section>
     )
 }
