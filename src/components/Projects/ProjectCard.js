8087dd90a19b7a8cd993f7c47e03c3b701180d3b
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-23 21:41:47 +0600

    project card component create

---
+++ src/components/Projects/ProjectCard.js
@@ -0,0 +1,13 @@
+export const ProjectCard = ({title,description,imgUrl}) => {
+ return (
+    <Col sm={6} md={4}>
+     <div className="proj-imgbx">
+       <img src={imgUrl} alt="proj-img" />
+       <div className="proj-text">
+          <h4>{title}</h4>
+          <span>{description}</span>
+       </div>
+     </div>
+    </Col>
+ )
+}
