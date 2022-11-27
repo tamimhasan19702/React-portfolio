dee044efd673b81cf5345879a94394071f5343b1
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-28 02:01:46 +0600

    handle submit method setting up

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -24,7 +24,23 @@ export const Contact = () => {
     }
 
     const handleSubmit = () => {
-
+    e.preventDefault();
+    setButtonText('Sending...');
+    let response = await fetch('http://localhost:3000/contact', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "Application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formDetails),
+    });
+    setButtonText('Send');
+    let result = response.json();
+    setFormDetails(formInitialDetails);
+    if(result.code === 200){
+      setStatus({ success: true, message: 'Message Sent Successfully'});
+    }else {
+      setStatus({ success: false, message: 'Something Went Wrong, Please Try Again Later.'})
+    }
     }
    
     return(
