1fb282ab1c1388833b7d756d27d9a50407f60214
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-28 02:13:43 +0600

    asynch await error  fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -23,7 +23,7 @@ export const Contact = () => {
         })
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText('Sending...');
     let response = await fetch('http://localhost:3000/contact', {
