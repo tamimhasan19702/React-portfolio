74ee7d42eb8df538d27e9f0a4469ac733190dc48
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-29 02:56:45 +0600

    newsletter updates

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,3 +1,32 @@
-export const Newsletter = () => {
+import { Alert } from "bootstrap"
+import { Col,Row } from "react-bootstrap"
 
+export const Newsletter = ({subscribe,status,message}) => {
+   
+   const handleSubmit = () => {
+
+   }
+   
+   
+    return (
+     <Col lg={12}>
+      <div className="newsletter-bx">
+        <Row>
+         <Col lg={12} md={6} xl={5}>
+         <h3>Subscribe to our Newsletter</h3>
+         {status === 'sending' && <Alert>Sending...</Alert>}
+         {status === 'error' && <Alert variant="danger">{message}</Alert>}
+         {status === 'success' && <Alert variant="success">{message}</Alert>}
+         </Col>
+         <Col md={6} xl={7}>
+         <form onSubmit={handleSubmit}>
+          <div className="new-email-bx">
+            <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address"/>
+          </div>
+         </form>
+         </Col>
+        </Row>
+      </div>
+     </Col>
+ )
 }
