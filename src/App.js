2aca728cb7591b82eb9a6a81b6aa6671987dbac7
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-30 01:17:07 +0600

    newsletter css updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Banner } from './components/Banner/Banner';
 import { Skills } from './components/Skills/Skills';
 import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
+import { Newsletter } from './components/Newsletter/Newsletter';
+import { Footer } from './components/Footer/Footer';
 
 
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
       <Skills/>
       <Projects />
       <Contact/>
+      <Newsletter/>
+      <Footer/>
     </div>
   )
 }
